31e8371441e1c0aee4873c91958f44f1a8bb6abe
catherine-o <catherine.ohara@outlook.com>
2019-11-03 13:01:55 -0700

    refactors CalculatorForm component to Container component to more efficiently bind style to tip percentage

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,14 +1,73 @@
 <template>
     <div class='container'>
-        <CalculatorForm />
+        <form id='calculator-form' @submit.prevent='validateInputs'>
+            <input type='text' id='input-bill' v-model='billTotal' placeholder='Total Bill'>
+            <select name='tip' v-model='tipPercent' id='tip'>
+                <option value='0' selected>Tip</option>
+                <option value='0.10'>10%</option>
+                <option value='0.15'>15%</option>
+                <option value='0.18'>18%</option>
+                <option value='0.20'>20%</option>
+            </select>
+            <input type='text' id='party-size' v-model='partySize' placeholder='Party Size'>
+            <div id='message'>{{ this.message }}</div>
+            <button>Calculate</button>
+        </form>
+        <Calculations :tip='tipAmount' :total='totalAmount'/>
     </div>
 </template>
 
 <script>
-import CalculatorForm from './CalculatorForm'
+import Cleave from 'cleave.js'
+import Calculations from './Calculations'
 export default {
+    data() {
+        return {
+            billTotal: null,
+            tipPercent: 0,
+            partySize: null,
+            tipAmount: 0,
+            totalAmount: 0,
+            message: ''
+        }
+    },
     components: {
-        CalculatorForm
+        Calculations
+    },
+    mounted() {
+        new Cleave('#input-bill', {
+            numeral: true,
+            numeralThousandsGroupStyle: 'thousand',
+        })
+        new Cleave('#party-size', {
+            numeral: true,
+            numeralThousandsGroupStyle: 'thousand',
+            numeralDecimalScale: 0
+        })
+    },
+    methods: {
+        validateInputs() {
+            if (!this.billTotal) {
+                this.message = 'please enter Total Bill amount'
+            } else if (isNaN(this.billTotal) || this.billTotal < 0) {
+                this.message = 'incorrect format in Total Bill'
+            } else if (this.tipPercent === 0) {
+                this.message = 'please select a Tip amount'
+            } else if (!this.partySize) {
+                this.message = 'please enter Party Size'
+            } else if (isNaN(this.partySize) || this.partySize < 1) {
+                this.message = 'incorrect format in Party Size'
+            } else {
+                this.message = ''
+                this.calculateTotals()
+            }
+        },
+        calculateTotals() {
+            let tipAmount = (this.billTotal * this.tipPercent) / this.partySize
+            let totalAmount = (+(this.billTotal) + +(tipAmount)) / this.partySize
+            this.tipAmount = tipAmount.toFixed(2)
+            this.totalAmount = totalAmount.toFixed(2)
+        }
     }
 }
 </script>
@@ -21,5 +80,33 @@ export default {
     display: flex;
     flex-direction: column;
     margin: 25px;
+    #calculator-form {
+        margin-bottom: 90px;
+        input {
+            font-size: 22px;
+            padding: 5px;
+            margin: 20px 0px 0 30px;
+            width: 77%;
+        }
+        #tip {
+            width: 81%;
+            height: 35px;
+            margin: 20px 0px 0 30px;
+            font-size: 20px;
+        }
+        #message {
+            text-align: center;
+            margin: 10px 0 -5px 0;
+            color: rgb(76, 150, 211);
+        }
+        button {
+            font-size: 22px;
+            padding: 5px;
+            float: right;
+            margin: 20px 30px;
+            background-color: rgb(250, 251, 252);
+            width: 110px;
+        }
+    }
 }
 </style>
