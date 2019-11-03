f1897a3265f02e392ecccd791e3f40e929587232
catherine-o <catherine.ohara@outlook.com>
2019-11-02 23:47:32 -0600

    displays tip and total fields, basic structure set up with no functionality

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -1,39 +1,52 @@
 <template>
-    <div class='container'>
+    <div class='all'>
         <h1>Tip Calculator</h1>
-        <form id='calculator'>
-            <input type='text' placeholder='Total Bill'>
-            <input type='text' placeholder='% of tip dropdown'>
-            <input type='text' placeholder='Party Size'>
-            <button>Calculate</button>
-        </form>
+        <div class='container'>
+            <form id='calculator-form'>
+                <input type='text' placeholder='Total Bill'>
+                <input type='text' placeholder='% of tip dropdown'>
+                <input type='text' placeholder='Party Size'>
+                <button>Calculate</button>
+            </form>
+            <div class='calculations'>
+            <p>Tip:</p>
+            <p>Total:</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss">
-.container {
-    margin: 15% auto;
-    width: 50%;
+.all {
+    margin: 5% auto;
+    width: 350px;
     h1 {
         text-align: center;
     }
-    #calculator {
+    .container {
+        background-color: rgb(239, 242, 243);
         border: 1px solid gray;
         border-radius: 5px;
         display: flex;
         flex-direction: column;
         margin: 25px;
-        input {
-            font-size: 22px;
-            padding: 5px;
-            margin: 20px 20px 0 20px;
+        #calculator-form {
+            input {
+                font-size: 22px;
+                padding: 5px;
+                margin: 20px 0px 0 35px;
+            }
+            button {
+                font-size: 22px;
+                padding: 5px;
+                margin: 20px 0 20px 150px; //float right not working
+                background-color: rgb(250, 251, 252);
+                width: 110px;
+            }
         }
-        button {
-            font-size: 22px;
-            padding: 5px;
-            margin: 20px 20px;
-            background: none;
-            width: 40%;
+        .calculations {
+            margin: 0 0 30px 35px;
+            font-size: 20px;
         }
     }
 }
