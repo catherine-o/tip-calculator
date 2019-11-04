285f8a9744d5b83191b8b3bde1b533d60ed41a80
catherine-o <catherine.ohara@outlook.com>
2019-11-03 21:00:38 -0700

    removes unused comments and code

--- src/test/container.test.js
+++ src/test/container.test.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import Container from '@/components/Container'
 
-function mountComponentWithProps (Component, propsData) {
-    const Constructor = Vue.extend(Component)
-    const vm = new Constructor({
-        propsData
-    }).$mount()
-
-    return vm.$el
-}
-
 describe('Container', () => {
     it('should render a Calculations component', () => {
         const vm = new Vue({
@@ -24,11 +15,3 @@ describe('Container', () => {
         
     })
 })
-
-// describe('Container', () => {
-//     it('should contain a form', () => {
-//         expect(document.querySelector('.container').innerHTML
-//             .toContain(Form)
-//         )
-//     })
-// })
